6041431efb3044b285c4411fb4fa989bdc20986e
ctghadiri <ctghadiri@gmail.com>
2019-06-13 14:42:04 -0700

    add more nav and adjust name and id

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -4,16 +4,28 @@ const Nav = () => {
     return (
         <div class="row">
         <div class="col s12 m9 l10">
-          <div id="introduction" class="section scrollspy">
-            <p>Content </p>
+          <div id="about-me" class="section scrollspy">
+            <p>About Me </p>
           </div>
     
-          <div id="structure" class="section scrollspy">
-            <p>Content </p>
+          <div id="skills" class="section scrollspy">
+            <p>Skills </p>
           </div>
     
-          <div id="initialization" class="section scrollspy">
-            <p>Content </p>
+          <div id="education" class="section scrollspy">
+            <p>Education </p>
+          </div>
+
+          <div id="experience" class="section scrollspy">
+            <p>Experience </p>
+          </div>
+
+          <div id="projects" class="section scrollspy">
+            <p>Projects </p>
+          </div>
+
+          <div id="contact" class="section scrollspy">
+            <a href="/contact"><p>Contact </p></a>
           </div>
         </div>
         <div class="col hide-on-small-only m3 l2">
