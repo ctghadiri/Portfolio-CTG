a5690e79cd1e363a4e90f17c3804ad5d777c757a
ctghadiri <ctghadiri@gmail.com>
2019-06-14 12:53:56 -0700

    add h1 tage for props.name

--- client/src/components/container/index.js
+++ client/src/components/container/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { checkPropTypes } from 'prop-types';
 
-const Container = () => {
+const Container = (props) => {
     return (
         <div>
-
+            <h1>{props.name}</h1>
         </div>
     )
 }
