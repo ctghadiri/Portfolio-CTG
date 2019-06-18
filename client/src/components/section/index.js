2fc45f0e2f2ea5c958ebf2d5dc25560b908ffdf6
ctghadiri <ctghadiri@gmail.com>
2019-06-18 12:09:55 -0700

    change class to className as a jsx attribute

--- client/src/components/section/index.js
+++ client/src/components/section/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Section = (props) => {
     return (
-        <div class="row scrollspy" id={props.id}>
+        <div className="row scrollspy" id={props.id}>
             <h1>{props.name}</h1>
         </div>
     )
