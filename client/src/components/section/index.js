2f3af81c7ba0b46743b1d6e25af23bd8162971cc
ctghadiri <ctghadiri@gmail.com>
2019-06-18 16:12:09 -0700

    comment out div with row class in section component and create projects file with general setup to export materialize grid for progimg

--- client/src/components/section/index.js
+++ client/src/components/section/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Section = (props) => {
     return (
-        <div className="container">
-            <div className="row scrollspy" id={props.id}>
+        <div className="container scrollspy" id={props.id}>
+            {/* <div className="row scrollspy" id={props.id}> */}
                 <h1>{props.name}</h1>
-            </div>
+            {/* </div> */}
         </div>
     )
 }
