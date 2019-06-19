cd611c6cfc1bf17c4de5c9faa1f34c8bc840f03e
ctghadiri <ctghadiri@gmail.com>
2019-06-19 16:23:54 -0700

    adjust props argument name

--- client/src/components/projects/details/index.js
+++ client/src/components/projects/details/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjDetails = (props) => {
     return (
         <div className="col s12 l8">
-            <h2>{props.proj-title}</h2>
+            <h2>{props.projTitle}</h2>
             {props.children}
         </div>
     )
--- client/src/components/projects/image/index.js
+++ client/src/components/projects/image/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjImg = (props) => {
     return (
         <div className="col s12 l4">
-            <img src={props.proj-img} className="responsive-img">
+            <img src={props.projImg} className="responsive-img">
             </img>
         </div>
     )
