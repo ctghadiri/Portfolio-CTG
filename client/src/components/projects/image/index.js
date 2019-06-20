62d0be77f1757fc6208e9d1527d71b5e9a9bd8aa
ctghadiri <ctghadiri@gmail.com>
2019-06-19 17:42:07 -0700

    add class project-img for styling purposes

--- client/src/components/projects/image/index.js
+++ client/src/components/projects/image/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 const ProjImage = (props) => {
     return (
         <div className="col s12 l4">
-            <img src={props.projImg} alt="" className="responsive-img">
-            </img>
+            <img src={props.projImg} alt="" className="responsive-img project-img"/>
         </div>
     )
 }
