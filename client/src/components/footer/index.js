35430ee6d3b28f4c1b2f4388e6fc76b8e97e1e9e
ctghadiri <ctghadiri@gmail.com>
2019-06-24 15:03:46 -0700

    center text and create rows

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer class="page-footer">
-        {/* <div class="footer-copyright"> */}
-          <div class="container">
-          © 2014 Copyright Text
+      <footer class="page-footer">
+        <div class="row">
+        </div>
+        <div class="row">
+          <div class="container center-align">
+          © Copyright
           <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
           </div>
-        {/* </div> */}
+        </div>
       </footer>
     )
 }
