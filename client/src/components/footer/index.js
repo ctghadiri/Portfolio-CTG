3002f49a5f9d64a4e3da72e440cd1242859701c2
ctghadiri <ctghadiri@gmail.com>
2019-06-26 14:07:42 -0700

    add .footer-margin to footer component and css to remove bottom margin

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 const Footer = () => {
     return (
       <footer class="page-footer">
         <div class="row">
         </div>
-        <div class="row">
+        <div class="row footer-margin">
           <div class="container center-align">
           © Copyright
           <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
