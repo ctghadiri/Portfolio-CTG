d78b0e40bc5710a2ee47f4b61a0e4deb430861fc
ctghadiri <ctghadiri@gmail.com>
2019-06-26 10:31:29 -0700

    add info in experience section

--- client/src/App.js
+++ client/src/App.js
@@ -26,16 +26,44 @@ function App() {
       </Section>
       <Section name="Education" id="education">
         <div className="row section">
-          <h6>University of Califonia, Berkeley Extension -- Full Stack Coding Bootcamp</h6>
+          <h6>University of Califonia, Berkeley Extension — Full Stack Coding Bootcamp</h6>
           <p>2019</p>
         </div>
         <div className="row section">
-          <h6>University of Califonia, Davis -- B.S. Economics</h6>
+          <h6>University of Califonia, Davis — B.S. Economics</h6>
           <p>2012</p>
         </div>
       </Section>
       <Section name="Experience" id="experience">
-
+        <div className="row section">
+          <h6>JPMorgan Chase, San Jose, CA — Relationship Banker</h6>
+          <ul>
+            <li>Troubleshoot client issues to resolve escalations in a timely manner.</li>
+            <li>Actively leverage current technologies to educate customers and recommend solutions that fit their needs.</li>
+            <li>Coach team members to develop sales opportunities and refer clients to designated business partners to meet sales goals.</li>
+          </ul>
+        </div>
+        <div className="row section">
+          <h6>Homesuite, Palo Alto, CA — Landlord and Partnership Account Manager</h6>
+          <ul>
+            <li>Responsible for seamlessly and quickly managing multi-channel interactions with current and prospective landlord partners.</li>
+            <li>Provided technical support and troubleshoot problems that arose with existing accounts.</li>
+          </ul>
+        </div>
+        <div className="row section">
+         <h6>GageIn, Mountain View, CA — Sales Associate</h6>
+         <ul>
+            <li>Conducted SaaS product demos for small to mid-market end-users via GoToMeeting and join.me.</li>
+            <li>Worked in a close-knit team brainstorming and implementing sales and marketing strategies</li>
+          </ul>
+        </div>
+        <div className="row section">
+         <h6>Maple Press, San Jose, CA — Account Manager</h6>
+         <ul>
+            <li>Managed key relationships with existing customers, prospects, and partners.</li>
+            <li>Prepared and presented marketing reports by collecting, analyzing, and summarizing sales data.</li>
+          </ul>
+        </div>
       </Section>
       <Section name="Projects" id="projects">
           <div className="row section">
