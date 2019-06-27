8149f6ecfa505cbfb568a53c6d5a9541fb17f342
ctghadiri <ctghadiri@gmail.com>
2019-06-26 23:36:52 -0700

    make project images hoverable

--- client/src/components/projects/image/index.js
+++ client/src/components/projects/image/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const ProjImage = (props) => {
     return (
-        <div className="col s12 l4">
+        <div className="col s12 l4 hoverable">
             <a href={props.link} target="_blank">
                 <img src={props.projImg} alt="" className="responsive-img project-img"/>
             </a>
