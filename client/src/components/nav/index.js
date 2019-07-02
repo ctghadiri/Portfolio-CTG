f492827042c57ac70d06d079c969091788e47fdd
ctghadiri <ctghadiri@gmail.com>
2019-07-02 13:56:49 -0700

    add styling to div in regards to margin

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,33 +1,9 @@
 import React from 'react';
+import './style.css';
 
 const Nav = () => {
     return (
-        <div class="row">
-        {/* <div class="col s12 m9 l10">
-          <div id="about-me" class="section scrollspy">
-            <p>About Me </p>
-          </div>
-    
-          <div id="skills" class="section scrollspy">
-            <p>Skills </p>
-          </div>
-    
-          <div id="education" class="section scrollspy">
-            <p>Education </p>
-          </div>
-
-          <div id="experience" class="section scrollspy">
-            <p>Experience </p>
-          </div>
-
-          <div id="projects" class="section scrollspy">
-            <p>Projects </p>
-          </div>
-
-          <div id="contact" class="section scrollspy">
-            <a href="/contact"><p>Contact </p></a>
-          </div>
-        </div> */}
+      <div class="row nav-margin">
         <a href="#" class="sidenav-trigger hide-on-large-only" data-target="mobile-links">
           <i class="material-icons">menu</i>
         </a>
