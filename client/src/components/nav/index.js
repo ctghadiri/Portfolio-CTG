caab5a27e065cae1a300cc62d49b0a22c0e46cf6
ctghadiri <ctghadiri@gmail.com>
2019-07-08 10:28:54 -0700

    make nav background grey with opacity .75, add class pinned to make nav stick to top and change text color to lighter color to make more visible

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -3,18 +3,18 @@ import './style.css';
 
 const Nav = () => {
     return (
-      <div class="row nav-margin pinned">
+      <div class="row nav-margin pinned nav-background">
         <a href="#" class="sidenav-trigger hide-on-large-only" data-target="mobile-links">
           <i class="material-icons">menu</i>
         </a>
         <div class="col hide-on-med-and-down m3 l2">
           <ul class="section table-of-contents">
-            <li><a href="#about-me">About Me</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#education">Education</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about-me" className="grey-text text-lighten-5">About Me</a></li>
+            <li><a href="#skills" className="grey-text text-lighten-5">Skills</a></li>
+            <li><a href="#education" className="grey-text text-lighten-5">Education</a></li>
+            <li><a href="#experience" className="grey-text text-lighten-5">Experience</a></li>
+            <li><a href="#projects" className="grey-text text-lighten-5">Projects</a></li>
+            <li><a href="#contact" className="grey-text text-lighten-5">Contact</a></li>
           </ul>
         </div>
         <ul class="sidenav" id="mobile-links">
