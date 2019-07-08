98f2e45a091b2d413ff26f3912465c2e6249c287
ctghadiri <ctghadiri@gmail.com>
2019-07-08 15:04:47 -0700

    change class for the section component to adjust color and change color in style.css

--- client/src/components/section/index.js
+++ client/src/components/section/index.js
@@ -6,7 +6,7 @@ const Section = (props) => {
         <div className="teal accent-2 section margin">
             <div className="container scrollspy" id={props.id}>
                 {/* <div className="row scrollspy" id={props.id}> */}
-                    <h1 className="orange-text text-darken-3">{props.name}</h1>
+                    <h1 className="section-text">{props.name}</h1>
                 {/* </div> */}
                 {props.children}
             </div>
