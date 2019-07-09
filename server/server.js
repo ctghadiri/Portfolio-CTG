6e0f7b4901d9a6bc5bbc580732d729b03268e2f8
ctghadiri <ctghadiri@gmail.com>
2019-07-09 11:21:05 -0700

    defien middleware, start api server, and add routes for server.js

--- server/server.js
+++ server/server.js
@@ -3,3 +3,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/portfolio")
+
+app.listen(PORT, function(){
+    console.log(`Server listening on PORT ${PORT}`)
+})
