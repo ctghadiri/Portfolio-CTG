e442f02234cf91711bb845cc04b5da0586ff8b57
ctghadiri <ctghadiri@gmail.com>
2019-07-09 11:33:31 -0700

    add comments for server.js

--- server/server.js
+++ server/server.js
@@ -6,13 +6,16 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+// define middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(routes);
 
+// connect to mongo db
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/portfolio")
 
+// start the API server
 app.listen(PORT, function(){
     console.log(`Server listening on PORT ${PORT}`)
 })
